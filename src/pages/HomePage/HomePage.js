6f6252e328d92d8c29aba5e08c5a686c22e97ffa
alex <reyz999@gmail.com>
2024-09-01 21:17:45 -0600

    first screen

--- src/pages/HomePage/HomePage.js
+++ src/pages/HomePage/HomePage.js
@@ -4,16 +4,38 @@ import { useSelector, useDispatch } from 'react-redux';
 import { increment, decrement } from '../../_helpers/store';
 import Box from "@mui/material/Box";
 import {styled} from "@mui/material";
+import FirstScreenImg from '../../images/firstScreen.jpg'
+
+const screenHeight = window.innerHeight;
+
+const FirstScreen = styled(Box)(({ theme }) => ({
+    height: screenHeight,
+    width: '100vw',
+    background:FirstScreenImg,
+    '& img': {
+        width:'100%',
+        height:'100%'
+    }
+}));
+
+const MainContainer = styled('Box')(({ theme }) => ({
+
+}));
 
 const Home = () => {
     const count = useSelector((state) => state.counter);
     const dispatch = useDispatch();
-    const MainContainer = styled('Box')(({ theme }) => ({
-        padding:'-60px 0 0 0'
-    }));
+
+    console.log('Высота видимой области экрана:', screenHeight, 'пикселей');
+    console.log(FirstScreenImg);
+
 
     return (
         <MainContainer>
+            <FirstScreen>
+                <img src={{FirstScreenImg}} alt="img"/>
+            </FirstScreen>
+
             <h2>Home</h2>
             <p>Welcome to the Home page!</p>
             <div style={{height:'1000px'}}>
