5974e53ce8fdb4d9cbe3e13ba716704e57b9bc33
alex <reyz999@gmail.com>
2024-12-22 20:50:09 -0700

    future project block

--- src/_components/FeatureProject.js
+++ src/_components/FeatureProject.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 import {styled} from "@mui/material";
 import { Box, Button, Typography } from '@mui/material';
@@ -6,14 +6,19 @@ import { Box, Button, Typography } from '@mui/material';
 
 import Grid from "@mui/material/Grid";
 import palette from '../../src/theme/palette';
+import useTheme from "@mui/material/styles/useTheme";
 
 const ProjectList = styled(Grid)(({ theme }) => ({
 
 }));
 
 
-const ProjectItem = styled(Grid)(({ theme }) => ({
+const ProjectItem = styled(Grid)(({ theme, isHovered  }) => ({
    position:'relative',
+    overflow: 'hidden',
+    height:'300px',
+    // flexBasis: isHovered ? '50%' : '25%', // Эквивалент sm=6 или sm=3
+    transition: 'flex-basis 0.3s ease', // Плавная анимация изменения размера
     'img': {
         width:'100%',
         height:'100%',
@@ -21,7 +26,7 @@ const ProjectItem = styled(Grid)(({ theme }) => ({
     }
 }));
 
-const Shadow = styled(Box)(({ theme }) => ({
+const Shadow = styled(Box)(({ theme, isHovered }) => ({
     position:'absolute',
     top:'0',
     left:'0',
@@ -32,25 +37,38 @@ const Shadow = styled(Box)(({ theme }) => ({
     height:'100%',
     background:'rgba(0,0,0, 0.5)',
     color:palette.mainColor,
-    cursor:'pointer'
+    cursor:'pointer',
+    opacity: isHovered ? 0 : 1, // Скрываем блок
+    visibility: isHovered ? 'hidden' : 'visible',
+    transition: 'opacity 0.3s ease, visibility 0.3s ease', // Плавное исчезновение
 
 }));
 
 
 const FutureProject = props => {
   const  {itemArr} = props;
+  const theme = useTheme();
+
+  const [hoveredIndex, setHoveredIndex] = useState(0);
 
   console.log(itemArr,'itemArr!!!!')
 
   return (
       <ProjectList container>
           {itemArr?.map((el, i) => (
-              <ProjectItem item sm={i === 0 ? 6 : 3}>
+              <ProjectItem item sm={hoveredIndex === i ? 6 : 3 }
+               key={i}
+               isHovered={hoveredIndex === i}
+               onMouseEnter={() => setHoveredIndex(i)}
+               onMouseLeave={() => setHoveredIndex(0)}
+              >
+                  {console.log(hoveredIndex,'-------hoveredIndex')}
                  <img src={el.image} alt={'img'}/>
-                <Shadow>
-                    <Typography variant='h6' color={palette.mainColor}>{el.name}</Typography>
-
-                </Shadow>
+                  {i !== 0 &&
+                      <Shadow isHovered={hoveredIndex === i}>
+                          <Typography variant='h6' color={palette.mainColor}>{el.name}</Typography>
+                      </Shadow>
+                  }
               </ProjectItem>
               ))}
 
